673287975f2ca26dd3918813bdad5138db93ec79
evanysilvestre <evany.120@gmail.com>
2025-09-11 19:54:26 -0300

    add attributes

--- src/exercises/Exercise1.java
+++ src/exercises/Exercise1.java
@@ -1,5 +1,28 @@
 package exercises;
+/*Write a Java program to create a class called "Book" with attributes for title, author, and ISBN, and methods to add and remove books from a collection.*/
+import java.util.Scanner
+
+class Book {
+	String title;
+	String author;
+	int isnb;
+}
+
+public Book(String title, String author, int isnb) {
+	this.title = title;
+	this.author = author;
+	this.isnb = isnb;
+}
+
+static void addBook() {
+	
+}
+
+static void removeBook(String title) {
+	
+}
 
 public class Exercise1 {
+	
 
 }
