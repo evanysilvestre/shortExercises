f15c608ee08363d610b15c3e271ac0cd56bb7df0
evanysilvestre <evany.120@gmail.com>
2025-09-13 21:55:00 -0300

    change structure folder

--- src/exercises/Exercise1.java
+++ src/exercises/Exercise1.java
@@ -1,54 +1,50 @@
 package exercises;
 /*Write a Java program to create a class called "Book" with attributes for title, author, and ISBN, and methods to add and remove books from a collection.*/
-import java.util.Scanner
-import java.util.List;
+import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.List;
 
 class Book {
 	String title;
 	String author;
 	String isnb;
+	
+	public Book(String title, String author, String isnb) {
+		this.title = title;
+		this.author = author;
+		this.isnb = isnb;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title: " + title + ", author:" + author + ", isnb:" + isnb + "]";
+	}
+
 }
-
-public Book(String title, String author, String isnb) {
-	this.title = title;
-	this.author = author;
-	this.isnb = isnb;
-}
-
-
-@Override
-public String toString() {
-    return "Book [Title:" + title + ", Author: " + author + ", ISNB: " + isnb + "]";
-}
-
 public class Exercise1 {
 	static Scanner scn = new Scanner(System.in);
 	static List<Book> books = new ArrayList<>();
 	
 	static void addBook(String title, String author, String isnb) {
-		static Book book = new Book(title, author, isnb);
+		Book book = new Book(title, author, isnb);
 		books.add(book);
-		
-		System.out.println()
+		System.out.println("Your book was added!");
+	}
+	
+	static void removeBookByTitle(String title) {
+		for (Book b: books) {
+			if (b.title.equalsIgnoreCase(title)) {
+				books.remove(b);
+				System.out.println("The book " + b.title + " was removed!");
+				break;
+			} else {
+				System.out.println("There is no book with this name. Try again.");
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
-		Syste.out.println("Title: ");
-		String title = scn.nextLine();
-		Syste.out.println("Author ");
-		String author = scn.nextLine();
-		Syste.out.println("ISNB: ");
-		String isnb = scn.nextLine();
-		
-		addBook(title, author, isnb);
 		
 	}
-	/*static void removeBookByTitle(String title) {
-		for (Book b: books) {
-			if (b.title.equalsIgnoreCase(title)) {
-				books.
-			}
-		}
-	}*/
+
 }
